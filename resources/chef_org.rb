2511d34aeee574ef63485e15f41889fbd1eab604
Håvard Lauve Hansen <havard.lauve.hansen@intility.no>
2025-03-25 10:09:07 +0100

    Refactor chef_org resource to improve user organization management and enhance idempotency checks

--- resources/chef_org.rb
+++ resources/chef_org.rb
@@ -29,10 +29,25 @@ property :admins, Array, required: true
 property :users, Array, default: []
 property :remove_users, Array, default: []
 property :key_path, String
+property :pivotal_key, String, default: '/etc/opscode/pivotal.pem' # Assumes pivotal key is in the default location
+property :client_rb, String, default: '/etc/chef/client.rb'
+
+Chef::Config.from_file("#{new_resource.client_rb}")
+property :chef_server_url, String, default: Chef::Config[:chef_server_url].gsub(%r{/organizations/.*}, '')
+
+# Intialize Chef::Config with pivotal
+Chef::Config[:chef_server_url] = "#{new_resource.chef_server_url}/organizations/#{new_resource.org}"
+Chef::Config[:client_name] = 'pivotal'
+Chef::Config[:client_key] = new_resource.pivotal_key
+api = Chef::ServerAPI.new(Chef::Config[:chef_server_url])
 
 load_current_value do
   node.run_state['chef-users'] ||= shell_out('chef-server-ctl user-list').stdout
   node.run_state['chef-orgs'] ||= shell_out('chef-server-ctl org-list').stdout
+  new_resource.users.each do |user|
+    node.run_state["#{user}_orgs"] ||= JSON.parse(shell_out("chef-server-ctl user-show #{user} -l -F json").stdout.strip)['organizations']
+  end
+  node.run_state["#{new_resource.org}_admins"] ||=
   current_value_does_not_exist! unless node.run_state['chef-orgs'].index(/^#{org}$/)
 end
 
@@ -53,7 +68,7 @@ action :create do
   end
 
   new_resource.users.each do |user|
-    org_user_exist = JSON.parse(shell_out("chef-server-ctl user-show #{user} -l -F json").stdout)['organizations'].include?(new_resource.org)
+    org_user_exist = node.run_state["#{user}_orgs"].include?(new_resource.org)
     execute "add-user-#{user}-org-#{new_resource.org}" do
       command "chef-server-ctl org-user-add #{new_resource.org} #{user}"
       only_if { node.run_state['chef-users'].index(/^#{user}$/) }
@@ -61,11 +76,11 @@ action :create do
     end
   end
 
-  # TODO: fix idempotency for org admins
   new_resource.admins.each do |user|
     execute "add-admin-#{user}-org-#{new_resource.org}" do
       command "chef-server-ctl org-user-add --admin #{new_resource.org} #{user}"
       only_if { node.run_state['chef-users'].index(/^#{user}$/) }
+      not_if { api.get("organizations/#{new_resource.org}/groups/admins")['users'].include?(user) }
     end
   end
 
